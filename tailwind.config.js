c7d0111ccdd9b8c3d8f7f31e9e398da8e57f8d4b
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-04 13:13:54 +0200

    added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000",
+      },
+    },
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+    },
   },
   plugins: [],
 };
