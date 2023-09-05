cda391375c51d92387c08a463aa76e79934d3972
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-06 00:23:32 +0200

    added booking status & error selector

--- src/app/store/bookings/bookings.selector.ts
+++ src/app/store/bookings/bookings.selector.ts
@@ -1,4 +1,13 @@
-import { createFeatureSelector } from "@ngrx/store";
-import { IBooking } from "src/app/models/IBooking";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { IBookingsState } from './bookings.reducer';
 
-export const selectBookings = createFeatureSelector<IBooking[]>('bookings');
+export const selectBookings = createFeatureSelector<IBookingsState>('bookings');
+
+export const selectBookingStatus = createSelector(
+  selectBookings,
+  (state: IBookingsState) => state.status
+);
+export const selectBookingError = createSelector(
+  selectBookings,
+  (state: IBookingsState) => state.error
+);
