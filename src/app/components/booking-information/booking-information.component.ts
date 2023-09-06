5b521e963ac10164b1547cc46ecf8791ba709980
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-06 17:11:51 +0200

    add dialog

--- src/app/components/booking-information/booking-information.component.ts
+++ src/app/components/booking-information/booking-information.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { IBookingResponse } from 'src/app/models/IBooking';
 import { IAppState } from 'src/app/store/app.state';
 import { BookingsActions } from 'src/app/store/bookings/bookings.actions';
+import { DeleteCheckComponent } from '../delete-check/delete-check.component';
 
 @Component({
   selector: 'app-booking-information',
@@ -13,7 +15,7 @@ export class BookingInformationComponent implements OnInit {
   @Input() currentBooking: IBookingResponse | undefined;
   date: Date | undefined;
 
-  constructor(private store: Store<IAppState>) {}
+  constructor(private store: Store<IAppState>, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     if (this.currentBooking) this.date = new Date(this.currentBooking.date);
@@ -21,5 +23,11 @@ export class BookingInformationComponent implements OnInit {
 
   resetCurrentBooking() {
     this.store.dispatch(BookingsActions.resetCurrentBooking());
+  }
+
+  openDialog() {
+    this.dialog.open(DeleteCheckComponent, {
+      data: { bookingId: this.currentBooking?._id },
+    });
   }
 }
