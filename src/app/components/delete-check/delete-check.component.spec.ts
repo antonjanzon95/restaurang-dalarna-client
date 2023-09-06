d18419caf02a114c6b655c8d28f0cfb2426c6278
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-06 17:11:15 +0200

    add delete safety check dialog

---
+++ src/app/components/delete-check/delete-check.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeleteCheckComponent } from './delete-check.component';
+
+describe('DeleteCheckComponent', () => {
+  let component: DeleteCheckComponent;
+  let fixture: ComponentFixture<DeleteCheckComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeleteCheckComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeleteCheckComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
