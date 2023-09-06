841b5ab5353d0c14b7b001c92d0c1243e3f06196
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-09-06 15:58:32 +0200

    add nav

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { routes } from 'src/app/app-routing.module';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent {
+  public navRoutes = routes;
+}
